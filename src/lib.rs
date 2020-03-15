ce172e5fac9e61c3c72b93b00cb62d62e8f7c390
Alex Burka <aburka@exyntechnologies.com>
2020-03-14 23:17:01 -0400

    encapsulate structs (closes #7)

--- src/lib.rs
+++ src/lib.rs
@@ -10,10 +10,6 @@
 #[macro_use]
 extern crate delegate;
 
-use std::str::{Chars, CharIndices};
-use std::iter::{Iterator, DoubleEndedIterator, FusedIterator};
-use std::mem::{transmute, uninitialized};
-
 /// Extension trait for String providing owned char and char-index iterators
 pub trait OwnedCharsExt {
     /// Gets an owning iterator over the chars (see `chars()`)
@@ -32,82 +28,91 @@ impl OwnedCharsExt for String {
     }
 }
 
-/// Iterator over the chars of a string (the string is owned by the iterator)
-#[derive(Debug)]
-pub struct OwnedChars {
-    s: String,
-    i: Chars<'static>,
+/// structs
+mod structs {
+    use std::str::{Chars, CharIndices};
+    use std::iter::{Iterator, DoubleEndedIterator, FusedIterator};
+    use std::mem::{uninitialized, transmute};
+
+    /// Iterator over the chars of a string (the string is owned by the iterator)
+    #[derive(Debug)]
+    pub struct OwnedChars {
+        s: String,
+        i: Chars<'static>,
+    }
+
+    /// Iterator over the chars of a string and their indices (the string is owned by the iterator)
+    #[derive(Debug)]
+    pub struct OwnedCharIndices {
+        s: String,
+        i: CharIndices<'static>,
+    }
+
+    macro_rules! impls {
+        ($owned_struct:ident, $target_struct:ident, $method: ident, $item: ty) => {
+            impl $owned_struct {
+                /// Create Self from a String, moving the String into Self
+                pub fn from_string(s: String) -> Self {
+                    unsafe {
+                        // First, move the string
+                        let mut owned = $owned_struct {
+                            s: s,
+                            i: uninitialized()
+                        };
+
+                        // Then, we can call .chars, which with have the same
+                        // lifetime of the owner. We need the transmute to "widen"
+                        // the lifetime into 'static which would allow us to store
+                        // it in the owner.
+                        owned.i = transmute::<$target_struct, $target_struct<'static>>(owned.s.$method());
+
+                        owned
+                    }
+                }
+
+                /// Consume this struct and return the contained String
+                pub fn into_inner(self) -> String {
+                    self.s
+                }
+
+                delegate! {
+                    target self.i {
+                        /// Borrow the contained String
+                        pub fn as_str(&self) -> &str;
+                    }
+                }
+            }
+
+            impl Iterator for $owned_struct {
+                type Item = $item;
+
+                delegate! {
+                    target self.i {
+                        fn next(&mut self) -> Option<$item>;
+                        fn count(self) -> usize;
+                        fn size_hint(&self) -> (usize, Option<usize>);
+                        fn last(self) -> Option<$item>;
+                    }
+                }
+            }
+
+            impl DoubleEndedIterator for $owned_struct {
+                delegate! {
+                    target self.i {
+                        fn next_back(&mut self) -> Option<$item>;
+                    }
+                }
+            }
+
+            impl FusedIterator for $owned_struct {}
+        };
+    }
+
+    impls!(OwnedChars, Chars, chars, char);
+    impls!(OwnedCharIndices, CharIndices, char_indices, (usize, char));
 }
 
-/// Iterator over the chars of a string and their indices (the string is owned by the iterator)
-#[derive(Debug)]
-pub struct OwnedCharIndices {
-    s: String,
-    i: CharIndices<'static>,
-}
-
-macro_rules! impls {
-    ($owned_struct:ident, $target_struct:ident, $method: ident, $item: ty) => {
-        impl $owned_struct {
-            /// Create Self from a String, moving the String into Self
-            fn from_string(s: String) -> Self {
-                unsafe {
-                    // First, move the string
-                    let mut owned = $owned_struct {
-                        s: s,
-                        i: uninitialized()
-                    };
-
-                    // Then, we can call .chars, which with have the same
-                    // lifetime of the owner. We need the transmute to "widen"
-                    // the lifetime into 'static which would allow us to store
-                    // it in the owner.
-                    owned.i = transmute::<$target_struct, $target_struct<'static>>(owned.s.$method());
-
-                    owned
-                }
-            }
-
-            /// Consume this struct and return the contained String
-            pub fn into_inner(self) -> String {
-                self.s
-            }
-
-            delegate! {
-                target self.i {
-                    /// Borrow the contained String
-                    pub fn as_str(&self) -> &str;
-                }
-            }
-        }
-
-        impl Iterator for $owned_struct {
-            type Item = $item;
-
-            delegate! {
-                target self.i {
-                    fn next(&mut self) -> Option<$item>;
-                    fn count(self) -> usize;
-                    fn size_hint(&self) -> (usize, Option<usize>);
-                    fn last(self) -> Option<$item>;
-                }
-            }
-        }
-
-        impl DoubleEndedIterator for $owned_struct {
-            delegate! {
-                target self.i {
-                    fn next_back(&mut self) -> Option<$item>;
-                }
-            }
-        }
-
-        impl FusedIterator for $owned_struct {}
-    };
-}
-
-impls!(OwnedChars, Chars, chars, char);
-impls!(OwnedCharIndices, CharIndices, char_indices, (usize, char));
+pub use structs::*;
 
 #[test]
 fn chars() {
